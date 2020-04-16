cf3a848048be0f08561ef892aa88e141261fad68
Zzhk <zzhk1995@gmail.com>
2020-04-16 18:36:38 +0800

    更新依赖

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const gmMultiPictureRouter = require('./routes/gmMultiPicture')
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
